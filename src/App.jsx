5c01d6c12282af482bec2ba71ea7b22b53f7c78b
raheleh sepehri <raheleh1392@gmail.com>
2024-10-22 16:26:29 +0330

    create new app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Model from "./components/Model";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model/>
+    </main>
+  );
+}
+
+export default App;
